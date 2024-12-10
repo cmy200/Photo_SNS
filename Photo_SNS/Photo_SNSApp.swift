6b55b0c7d53ecb163c736198117971a057de0f17
cmy_200 <dog_cat_dog@naver.com>
2024-12-10 20:01:36 +0900

    Initial Commit

---
+++ Photo_SNS/Photo_SNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_SNSApp.swift
+//  Photo_SNS
+//
+//  Created by 최명연 on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_SNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
